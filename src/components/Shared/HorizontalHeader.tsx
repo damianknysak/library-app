0186616369bc32d072607245d017aa8e6b47f70e
damianknysak <juniorhakeroriginal@gmail.com>
2023-11-24 20:50:07 +0100

    url-fix

--- src/components/Shared/HorizontalHeader.tsx
+++ src/components/Shared/HorizontalHeader.tsx
@@ -53,7 +53,7 @@ const HorizontalHeader: React.FC = () => {
             <>
               <button
                 onClick={() => {
-                  searchParams.set("authorize", "register");
+                  searchParams.set("authorize", "login");
                   setSearchParams(searchParams);
                 }}
                 className="flex-1 h-14 bg-[--primary] rounded-xl flex space-x-2 items-center justify-center"
